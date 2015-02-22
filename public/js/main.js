3ed3dbe4a6844d4e47472d1a4bce80acf49040a9
Kevin Zurek <kevinzurek@gmail.com>
2015-02-21 19:16:10 -0500

    create static webpage

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+﻿
+$(document).ready(function () {
+    var navTracker = $('#navTracker').val();
+    $('.selected').removeClass("selected");
+    $('#' + navTracker).addClass('selected');
+
+    var subNavTracker = $('#subNavTracker').val();
+    $('.subSelected').removeClass("subSelected");
+    $('#' + subNavTracker).addClass("subSelected");
+
+    
+});
